097e3e1ade834860bd7aad9d95c1ea43f445122f
denniszubov <denniszubov365@gmail.com>
2020-07-12 17:36:10 +0200

    Better front end

--- music/models.py
+++ music/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 # Create your models here.
 class Song(models.Model):
     artist = models.ForeignKey(User, on_delete=models.CASCADE)
     song_name = models.CharField(max_length=50)
+    date_uploaded = models.DateTimeField(default=timezone.now)
     # audio_file = ...
     # tags = list field
     # cover_art = ...
